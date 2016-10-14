df8d468473f5e6c598f103db9654a0ffc5cb4f22
Chris <cr217@st-andrews.ac.uk>
2016-10-14 23:41:42 +0100

    Command Template for LoL Schedule

---
+++ index.js
@@ -0,0 +1,80 @@
+/**
+    Copyright 2014-2015 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+
+    Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance with the License. A copy of the License is located at
+
+        http://aws.amazon.com/apache2.0/
+
+    or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+*/
+
+/**
+ * This simple sample has no external dependencies or session management, and shows the most basic
+ * example of how to create a Lambda function for handling Alexa Skill requests.
+ *
+ * Examples:
+ * One-shot model:
+ *  User: "Alexa, tell Hello World to say hello"
+ *  Alexa: "Hello World!"
+ */
+
+/**
+ * App ID for the skill
+ */
+var APP_ID = undefined; //replace with "amzn1.echo-sdk-ams.app.[your-unique-value-here]";
+
+/**
+ * The AlexaSkill prototype and helper functions
+ */
+var AlexaSkill = require('./AlexaSkill');
+
+/**
+ * HelloWorld is a child of AlexaSkill.
+ * To read more about inheritance in JavaScript, see the link below.
+ *
+ * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Introduction_to_Object-Oriented_JavaScript#Inheritance
+ */
+var ESportsReports = function () {
+    AlexaSkill.call(this, APP_ID);
+};
+
+// Extend AlexaSkill
+ESportsReports.prototype = Object.create(AlexaSkill.prototype);
+ESportsReports.prototype.constructor = ESportsReports;
+
+ESportsReports.prototype.eventHandlers.onSessionStarted = function (sessionStartedRequest, session) {
+    console.log("ESportsReports onSessionStarted requestId: " + sessionStartedRequest.requestId
+        + ", sessionId: " + session.sessionId);
+    // any initialization logic goes here
+};
+
+ESportsReports.prototype.eventHandlers.onLaunch = function (launchRequest, session, response) {
+    console.log("ESportsReports onLaunch requestId: " + launchRequest.requestId + ", sessionId: " + session.sessionId);
+    var speechOutput = "Would you like know the League of Legends schedule";
+    var repromptText = "You can say ask me about the League of Legends schedule";
+    response.ask(speechOutput, repromptText);
+};
+
+ESportsReports.prototype.eventHandlers.onSessionEnded = function (sessionEndedRequest, session) {
+    console.log("ESportsReports onSessionEnded requestId: " + sessionEndedRequest.requestId
+        + ", sessionId: " + session.sessionId);
+    // any cleanup logic goes here
+};
+
+ESportsReports.prototype.intentHandlers = {
+    // register custom intent handlers
+    "GetLoLSchedule": function (intent, session, response) {
+        response.tellWithCard("The League of Legends schedule is!", "The League of Legends schedule is", "The League of Legends schedule is!");
+    },
+    "AMAZON.HelpIntent": function (intent, session, response) {
+        response.ask("You can say ask me about the League of Legends schedule!", "You can say ask me about the League of Legends schedule!");
+    }
+};
+
+// Create the handler that responds to the Alexa Request.
+exports.handler = function (event, context) {
+    // Create an instance of the HelloWorld skill.
+    var eSportsReports = new ESportsReports();
+    eSportsReports.execute(event, context);
+};
+
