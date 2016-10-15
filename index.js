bbcf63f68eb7feafc4ce01574bfd128b9541b8ed
Chris <cr217@st-andrews.ac.uk>
2016-10-15 02:11:18 +0100

    Adding local json object variables to test functinality

--- index.js
+++ index.js
@@ -64,7 +64,98 @@ ESportsReports.prototype.eventHandlers.onSessionEnded = function (sessionEndedRe
 ESportsReports.prototype.intentHandlers = {
     // register custom intent handlers
     "GetLoLSchedule": function (intent, session, response) {
-        getEsportsSchedule();
+        //getEsportsSchedule2();
+
+        //var noaaResponseObject = JSON.parse(noaaResponseString);
+        var jsonObject = {"id":1,"name":"Cloud9 ","acronym":"C9","roster":[
+            {"id":24,
+                "name":"Bunny FuFuu","first_name":"Michael","last_name":"Kurylo","role":"Support",
+                "bio":"Known for his Thresh picks, Michael Bunny Fufuu Kurylo is a long time support " +
+                "player in the NA scene. Bunny Fufuu originally played support for Curse during the 2014 NA " +
+                "LCS Spring Split before moving to Curse Academy (now Gravity). Bunny Fufuu has continued to pull" +
+                " out his aggressive moves in the bot lane as Gravity battled through their debut season in the NA " +
+                "LCS.","hometown":null},
+            {"id":13,"name":"Incarnati0n","first_name":"Nicolaj","last_name":"Jensen",
+                "role":"Mid Lane","bio":"Nicolaj Incarnati0n Jensen is a high elo mid laner from Denmark known " +
+            "for favoring assassin champions like Zed and Fizz. Incarnati0n launched his esports career with " +
+            "Team Solo Mebdi in 2012 competing in the Season 3 EU LCS Qualifier. However, the team was disqualified" +
+            " from the tournament and disbanded in early 2013. Incarnati0n himself was banned in 2013 due to verbal " +
+            "abuse and other unsportsmanlike behavior. When his ban was rescinded in mid 2015, Incarnati0n was almost" +
+            " immediately acquired by Cloud9.","hometown":null},
+            {"id":166,"name":"Hai","first_name":"Hai","last_name":"Lam",
+                "role":"Mid Lane","bio":"Hai “Hai” Lam is a veteran mid laner. Preferring assassin champs, Hai is happiest" +
+            " when he can explode the enemy carry instantly to turn team fights in Cloud9’s favor. After suffering a" +
+            " collapsed lung in 2014 Hai was forced to take a brief recovery apart from Cloud9, but he returned with " +
+            "a vengeance to lead Cloud9 to victory in IEM San Jose 2015. Hai has continued to push the Cloud9 " +
+            "organization forward both on and off The Rift since he retired from competitive play in 2015.","hometown":null},
+            {"id":18,"name":"Sneaky","first_name":"Zachary ","last_name":"Scuderi","role":"AD Carry",
+                "bio":"Once the AD Carry for Team Dignitas’ B Team, Zachary “Sneaky” Scuderi joined Cloud9 " +
+                "when they were still Quantic Gaming in 2013.. Sneaky became known for his Ashe/Zyra bot lane" +
+                " in combination with Lemonnation during Season 3, a deadly duo of crowd control and late game" +
+                " damage. Since then Sneaky has transitioned into a fantastic all-around AD carry. He now plays" +
+                " a strong Corki and Graves, and a world-class Lucian. Consistently earning one of the highest " +
+                "KDA ratios in the NA LCS, Sneaky has been instrumental in Cloud9’s rise to prominence.","hometown":null},
+            {"id":53,"name":"Smoothie","first_name":"Andy","last_name":"Ta","role":"Support","bio":"Andy “Smoothie”" +
+            " Ta joined Team Dragon Knights in 2015. Despite initial communication issues with his Korean bot lane " +
+            "partner LouisXGeeGee, Smoothie and TDK fought their way through the NA Challenger Series and earned a " +
+            "spot in the 2015 NA LCS Summer Split. Smoothie will need to level up his synergy with the new TDK AD " +
+            "carry Emperor if TDK are going to survive the NA LCS.","hometown":null},
+            {"id":118,"name":"Link","first_name":"Austin","last_name":"Shin","role":"Mid Lane","bio":"Austin Link " +
+            "Shin replaced veteran member Bigfatlp in the mid lane during late December 2012 after just two weeks as " +
+            "his substitute. Suddenly thrown into the fierce competition of the North American LCS, Link quickly " +
+            "adapted using his previous experience from professional teams, utilizing his versatile play style to " +
+            "stay alive against some of the best mid lane players in the world.  With many other roster adjustments," +
+            " Link is now one of the oldest members left.   The 2014 spring split saw a reinvigorated Link and " +
+            "revitalized CLG. Playing some of the most promising games in recent memory, Link helped his team finish" +
+            " the split in a solid third place. In a dramatic turn of events leading into the 2014 All-Star event, " +
+            "Link was traded to Cloud 9 to relieve an injured and ailing Hai as he recovered from a collapsed lung. " +
+            "Although Cloud 9 was knocked out during the semi finals against OMG, Link demonstrated that he is a " +
+            "world class mid laner.\r\n\r\nLuckily for Link, visiting Paris was just icing on the cake. Hardened " +
+            "from battle on the Field of Justice, and armed with new-found knowledge of Cloud 9’s inner workings, " +
+            "Link may be just the person to catapult CLG to a first place finish this summer split.","hometown":"North" +
+            " Hills, CA"},
+            {"id":39,"name":"Rush","first_name":"Yoonjae","last_name":"Lee","role":"Jungler","bio":"A relatively new " +
+            "player in the NA scene, Yoonjae “Rush” Lee moved from Korea to join LMQ in 2014. When LMQ was rebranded " +
+            "as Team Impulse, Rush was kept on the roster as their starting jungler. With a reputation for incredibly " +
+            "aggressive plays, Rush generally either dominates his enemies with early ganks or give up a slew of deaths." +
+            " Known for his Lee Sin and Vi in the NA LCS, Rush is going to take any opening to dive the backline and " +
+            "explode a carry.","hometown":null},
+            {"id":25,"name":"LemonNation","first_name":"Daerek","last_name":"Hart","role":"Support","bio":"Along with" +
+            " Hai, Daerek “LemonNation” Hart is one of the founding members of Cloud9. As the official “Carrier of the" +
+            " Secret Notebook,” LemonNation is widely seen as Cloud9’s strategist. However, don’t let the book fool you " +
+            "– LemonNation is one of the best supports in the NA LCS when it comes to early game map and lane control. " +
+            "LemonNation continues to import unexpected bot lane champions such as Nautilus and Veigar into the NA LCS " +
+            "from other regions.","hometown":"Eden Prairie"},{"id":8,"name":"Meteos","first_name":"William",
+                "last_name":"Hartman","role":"Jungler","bio":"William “Meteos” Hartman is the jungler for Cloud9. " +
+                "A purveyor of experimental jungle champions, Meteos has helped popularize a farm-heavy jungle style" +
+                " that relies on out-scaling his opponent in the mid and late game. By combining that with an uncanny" +
+                " intuition for countering his opposing jungler’s ganks, Meteos quickly rose to MVP status in the LCS." +
+                " However, the NA LCS has become more competitive than ever, and C9 is no longer guaranteed their spot" +
+                " at the top. ","hometown":"Fairfax"},
+            {"id":2,"name":"Balls","first_name":"An","last_name":"Le","role":"Top Lane","bio":"One of the best top " +
+            "laners in North America, Balls helped Cloud9 to an astonishing 25-3 record in their debut NA LCS season " +
+            "and is a huge factor in their ongoing success. Balls is also known for being on the of best Rumble players" +
+            " in the world, with a 100% win rate on the Mechanized Menace over the course of the 2014 and 2015 NA LCS." +
+            " The rest of the top lane field will need to be prepared, because Balls isn’t one to back down from a" +
+            " challenge.","hometown":"Pharr"},
+            {"id":32,"name":"Impact","first_name":"Eon-Young","last_name":"Jeong","role":"Top Lane","bio":"One " +
+            "of two former World Champions currently playing in the NA LCS (the other being Team Liquid’s Piglet)," +
+            " Eon-Young “Impact” Jeong is one of the most formidable top laners in the world. Impact rose to fame " +
+            "while playing with SK Telecom T1 K in 2013. With a squad packed with talent including Faker, bengi, " +
+            "and Piglet, SK Telecom T1 K claimed 1st place at the Season 3 World Championship. Impact was acquired" +
+            " by Team Impulse in the 2015 NA LCS Spring Split Preseason and helped the team achieve a 4th place " +
+            "finish in their debut season.","hometown":"South Korea"},
+            {"id":608,"name":"Jensen","first_name":"Nicolaj","last_name":"Jensen","role":"midlane",
+                "bio":null,"hometown":"Denmark"}],
+            "image":{"image":{"url":"/uploads/open-uri20160803-18976-1lxe9in",
+                "thumb":{"url":"/uploads/thumb_open-uri20160803-18976-1lxe9in"}}}};
+
+        var cloud9Array = jsonObject.roster;
+        var bunny = cloud9Array[0];
+
+
+        response.tellWithCard(bunny.bio,
+            bunny.bio, bunny.bio);
     },
     "AMAZON.HelpIntent": function (intent, session, response) {
         response.ask("You can say ask me about the League of Legends schedule!", "You can say ask me about the League of Legends schedule!");
@@ -90,18 +181,14 @@ function getEsportsSchedule() {
         });
 
         res.on('end', function () {
-            var noaaResponseObject = JSON.parse(noaaResponseString);
 
-            if (noaaResponseObject.error) {
-                console.log("NOAA error: " + noaaResponseObj.error.message);
-            } else {
-                response.tellWithCard("The League of Legends schedule is!",
-                    "The League of Legends schedule is", "The League of Legends schedule is!");
-            }
+
+
+
+            response.tellWithCard("The League of Legends schedule is!",
+                "The League of Legends schedule is", "The League of Legends schedule is!");
         });
-    }).on('error', function (e) {
-        console.log("Communications error: " + e.message);
-    });
+    })
 }
 
 function getEsportsSchedule2() {
@@ -119,22 +206,24 @@ function getEsportsSchedule2() {
         }
     };
 
+
+
     var data;
 
-    var req = http.request(options, function (res) {
-        var chunks = [];
+    var req = http.request('http://api.pandascore.co/lol/v1/matches/1', function (res) {
+        var noaaResponseString = '';
 
-        res.on("data", function (chunk) {
-            chunks.push(chunk);
+        res.on("data", function (noaaResponseString) {
+            noaaResponseString += data;
         });
 
         res.on("end", function () {
-            var body = Buffer.concat(chunks);
-            data = body.toString();
+            var noaaResponseObject = JSON.parse(noaaResponseString);
+
+            response.tellWithCard("The League of Legends schedule is!",
+                "The League of Legends schedule is", "The League of Legends schedule is!");
         });
     });
 
     req.end();
-
-    var serializedData = JSON.parse(data);
 }
